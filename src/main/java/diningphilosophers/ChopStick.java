a2333f9b65edcfcf7d1134f93514372037d51335
Rita-Chraibi <rita.chraibi.0@gmail.com>
2024-03-15 15:48:59 +0100

    first commit

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -1,36 +1,46 @@
 package diningphilosophers;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ChopStick {
 
-    private static int stickCount = 0;
+    private static int Count = 0;
     private boolean iAmFree = true;
-    private final int myNumber;
+    private final int Number;
+    private final static Lock lock = new ReentrantLock();
 
     public ChopStick() {
-        myNumber = ++stickCount;
+        Number = ++Count;
     }
 
-    synchronized public boolean tryTake(int delay) throws InterruptedException {
+    public boolean tryTake(int delay) throws InterruptedException {
         if (!iAmFree) {
-            wait(delay);
-            if (!iAmFree) // Toujours pas libre, on abandonne
+            boolean locked = lock.tryLock(300, java.util.concurrent.TimeUnit.MILLISECONDS);
+            if (!locked) 
             {
-                return false; // Echec
+                return false; 
             }
         }
+        else{
+            lock.lock();
+        }
         iAmFree = false;
-        // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
-        return true; // Succès
+        return true; 
     }
 
-    synchronized public void release() {
-        iAmFree = true;
-        notifyAll();
-        System.out.println("Stick " + myNumber + " Released");
+    public void release() {
+        try {
+            iAmFree = true;
+            System.out.println("Stick " + Number + " Released");
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public String toString() {
-        return "Stick#" + myNumber;
+        return "Stick#" + Number;
     }
 }
